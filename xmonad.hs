2082839ed10d82d634a122f6500aa6a7f3f5e44d
Correl Roush <croush@coredial.com>
2011-10-14 10:49:25 -0400

    Initial

---
+++ xmonad.hs
@@ -0,0 +1,1 @@
+xmonad.standalone.hs
---
+++ xmonad.standalone.hs
@@ -0,0 +1,33 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+myManageHook = composeAll
+    [ resource  =? "Do"        --> doIgnore
+    , className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+
+myBorderWidth = 2
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                        <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        {-, modMask = mod4Mask     -- Rebind Mod to the Windows key-}
+        {-, handleEventHook = fullScreenEventHook-}
+        , terminal = "terminator"
+        } `additionalKeys`
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
